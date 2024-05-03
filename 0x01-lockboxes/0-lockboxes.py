2ff26421ee0894be2d39ebecbbc6644b7d4cb8fe
Blessed Oigbochie <eprince1800@gmail.com>
2024-05-03 12:07:59 +0100

    improvement to the complexity using set for easy elemnet checking

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,17 +2,16 @@
 """this mdoule contains one function"""
 
 def canUnlockAll(boxes):
-    """method that determines if all the boxes can be opened.
-    args: boxes is a list of lists
+    """Method that determines if all the boxes can be opened.
+    Args: boxes is a list of lists
     Return: True if all boxes can be opened, else return False
     """
-    keys = [0]
-    for open_box in keys:
-        if len(keys) == len(boxes):
-            return True
-        for key in boxes[open_box]:
-            if key not in keys:
-                keys.append(key)
-    if len(keys) == len(boxes):
-        return True
-    return False
+    keys = {0}
+    opened_boxes = {0}
+    while keys:
+        box = keys.pop()
+        for key in boxes[box]:
+            if key not in opened_boxes:
+                keys.add(key)
+                opened_boxes.add(key)
+    return len(opened_boxes) == len(boxes)
