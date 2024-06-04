0310e675e3c1b5854c24fd70cbec1c4b1246be74
Oigbochie Blessed <eprince1800@gmail.com>
2024-06-04 21:17:00 +0100

    task star wars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/people/' + process.argv[2];
+
+request(url, async function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.name);
+  }
+});
